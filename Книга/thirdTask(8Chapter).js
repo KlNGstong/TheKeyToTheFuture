709641900a19b76e46657e7c7f84d3265bab521b
Goraerushenko <gora.erushenko@gmail.com>
2018-11-20 22:20:17 +0200

    thrdTask 8Chapter

--- Книга/thirdTask(8Chapter).js
+++ Книга/thirdTask(8Chapter).js
@@ -10,24 +10,25 @@ let allStates = []
 const numberOfStates = {}
 while(allStates.length < 8){
     const KeysAll = Object.keys(allStations)
-    for (let j = 0; j < KeysAll.length; j++) {
+    KeysAll.map(function(index,j){
         const IKeys = allStations[KeysAll[j]]
-        for (let i = 0; i < IKeys.length; i++) {
+        IKeys.map(function (index,i){
             if (allStates.includes(IKeys[i])){
                 IKeys.splice(i,1)
             } else {
                 numberOfStates[KeysAll[i]] = IKeys.length
             }
-        }
-    }
+        })
+    })
+        
     let min = numberOfStates[KeysAll[0]]
     let minStation = KeysAll[0]
-    for (let m = 0; m < KeysAll.length; m++) {
+    KeysAll.map(function(index,m) {
         if(min < allStations[KeysAll[m]].length){
             min = allStations[KeysAll[m]].length
             minStation = KeysAll[m]
         }
-    }
+    })
     allStates = allStates.concat(allStations[minStation])
     delete allStations[minStation]
 }
