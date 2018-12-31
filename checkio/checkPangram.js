9ca5badb0e4e0f1022ddb1279bcf2c35c5493e7a
Goraerushenko <gora.erushenko@gmail.com>
2018-12-31 17:11:25 +0200

    pangram

--- checkio/checkPangram.js
+++ checkio/checkPangram.js
@@ -1,16 +1,15 @@
 const ALPHABET = "abcdefghijklmnopqrstuvwxyz".split("");
-
 function checkPangram(text){
-    var ALPHABETobj = {}
-    var data = text.replace(/[^a-zA-Z]/g, '').toLowerCase().split("")
-    for (let i = 0; i < data.length; i++) {
-        ALPHABETobj[data[i]] = true
-    }
-    for (let i = 0; i < ALPHABET.length; i++) {
-        if (ALPHABETobj[ALPHABET[i]] !== true || !ALPHABET.length >= data.length) {
-            return false
+    let contrl = []
+    const data = text.replace(/[^a-zA-Z]/g, '').toLowerCase().split("")
+    data.forEach(function(letter){
+        if(!contrl.includes(letter)){
+            contrl.push(letter)
         }
+    })
+    if(contrl.length == ALPHABET.length ){
+        return true;
     }
-    return true;
+    return false
 }
-console.log(checkPangram("abcefghijklmnopqrstuvwxyz"));
+console.log(checkPangram("The quick brown fox jumps over the lazy dog."));
