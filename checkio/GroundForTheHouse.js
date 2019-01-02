61b2e566ab1e43670ff71c4b5ae04e22a2566224
Goraerushenko <gora.erushenko@gmail.com>
2019-01-02 22:48:08 +0200

    neares

--- checkio/GroundForTheHouse.js
+++ checkio/GroundForTheHouse.js
@@ -2,7 +2,9 @@ function house(plan) {
     const data = plan.split("\n")
     const InWide = []
     const InHeight = [];
-    data[0] == "" ? data.shift() : undefined    
+    if(data[0] == ""){
+        data.shift()  
+    }
     data.forEach((und,i) =>{
         data[i].split("").forEach((und,j) =>{
             data[i][j] == "#" ? (InWide.push(j+1) , InHeight.push(i)) : undefined 
@@ -14,8 +16,12 @@ function house(plan) {
         return 0
     }
     InWide.forEach(function(index){
-        index > max ? max = index : undefined
-        min > index ? min = index : undefined
+        if(index > max){
+            max = index 
+        }
+        if(min > index){
+            min = index 
+        } 
     });
     return ((InHeight[InHeight.length-1]+1 - InHeight[0]) * (max - (min-1)));
 }
