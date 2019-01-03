aeff8b50b354233d7ada2978f8a64973ecaf3009
Goraerushenko <gora.erushenko@gmail.com>
2019-01-03 15:11:19 +0200

    house

--- checkio/GroundForTheHouse.js
+++ checkio/GroundForTheHouse.js
@@ -5,27 +5,23 @@ function house(plan) {
     if(data[0] == ""){
         data.shift()  
     }
-    data.forEach((und,i) =>{
-        data[i].split("").forEach((und,j) =>{
-            if(data[i][j] == "#"){
-                InWide.push(j+1) 
+    let max_min = [0,data[0].length]
+    data.forEach((str,i) =>{
+        data[i].split("").forEach((sumb,j) =>{
+            if(sumb == "#"){
+                if (max_min[0] < j){
+                    max_min.splice(0,1,j+1)
+                }
+                if(max_min[1] > j){
+                    max_min.splice(1,1,j)
+                }
                 InHeight.push(i)
             }
         })
     })
-    let max = 0
-    let min = InWide[0]
     if(InHeight[0] == undefined){
         return 0
     }
-    InWide.forEach(function(index){
-        if(index > max){
-            max = index 
-        }
-        if(min > index){
-            min = index 
-        } 
-    });
-    return ((InHeight[InHeight.length-1]+1 - InHeight[0]) * (max - (min-1)));
+    return ((InHeight[InHeight.length-1]+1 - InHeight[0]) * (max_min[0]+1 - (max_min[1])));
 }
 console.log(house("\n0000000\n##00##0\n######0\n##00##0\n#0000#0\n"));
