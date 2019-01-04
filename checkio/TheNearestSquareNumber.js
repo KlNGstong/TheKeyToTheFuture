08d6beed706264a93ad7a0dc3d6bd4da32eefccb
Goraerushenko <gora.erushenko@gmail.com>
2019-01-04 14:15:35 +0200

    nearest

--- checkio/TheNearestSquareNumber.js
+++ checkio/TheNearestSquareNumber.js
@@ -2,14 +2,16 @@ let nNumber = [999, 999]
 function nearestSquare(number) {
     for (let i = 1; i < number; i++) {
         const FNumber  = i*i
-        if(FNumber > number){
-            if(nNumber[0] > FNumber - number){
+            if(nNumber[0] > FNumber - number && FNumber > number){
                 nNumber = [FNumber - number , FNumber]
+<<<<<<< nearest
             }
         } else {
             if(nNumber[0] > number - FNumber){
+=======
+            } else if(nNumber[0] > number - FNumber){
+>>>>>>> local
                 nNumber = [number - FNumber , FNumber]
-            }
         }
     }
     return nNumber[1];
