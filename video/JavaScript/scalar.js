2ac93852d7a3773efede5e552288e77d38a25abf
Goraerushenko <gora.erushenko@gmail.com>
2019-02-09 23:57:19 +0700

    javaScript

--- video/JavaScript/scalar.js
+++ video/JavaScript/scalar.js
@@ -7,4 +7,8 @@ console.dir({scalar1,scalar2})
 const object1 = { field: 5 }
 const object2 = object1
 object1.field = 6
-console.dir ({ object1,object2})
+console.dir ({ object1, object2 })
+
+// dir / log
+//console.log печатает элемент в дереве, подобном HTML
+//console.dir печатает элемент в дереве, подобном JSON
