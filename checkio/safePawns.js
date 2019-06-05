b2d6df0595c14e302aab0cc01cb7b2914a89fe2c
Goraerushenko <gora.erushenko@gmail.com>
2019-06-05 15:20:11 +0300

    safePawns

--- checkio/safePawns.js
+++ checkio/safePawns.js
@@ -1,12 +1,14 @@
 function safePawns(data) {
     const letters = ["a","b","c","d","e","f","g","h"]
     let count = 0
-    for (let i = 0; i < data.length; i++) {
-        const fast = letters.indexOf(data[i][0])
-        if(data.includes((letters[fast-1] + String(+data[i][1]-1))) || data.includes(letters[fast+1] + String(+data[i][1]-1))){
+    data.forEach(function (curValue,i) {
+        const fast = letters.indexOf(curValue[0])
+        let short =  String(+curValue[1]-1)
+        if(data.includes(letters[fast-1] + short) || 
+           data.includes(letters[fast+1] + short)){
             count++
-        }
-    }
+        }    
+    }); 
     return count
 }
-console.log(safePawns(["b4","c4","d4","e4","f4","g4","e5"]));
+console.log(safePawns(["b4", "d4", "f4", "c3", "e3", "g5", "d2"]));
