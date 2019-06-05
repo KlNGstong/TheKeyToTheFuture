fb8fc124da392773926123a5a6aab3c2bffb8e37
Goraerushenko <gora.erushenko@gmail.com>
2019-06-05 20:21:53 +0300

    third

--- checkio/isAscending.js
+++ checkio/isAscending.js
@@ -1,7 +1,14 @@
 function isAscending(items) {
-    items.forEach(function name(curValue, i){
-        return items[0] >= items[i++]
-    });
-    return items.some(name);
+    let ret = items.map(function (curValue,i) {
+        if(items[0] >= items[i++]){
+            return false
+        } else {
+            return true
+        }
+    })
+    if(ret.splice(1,items.length).includes(false)){
+        return false;
+    }
+    return true;
 }
-console.log(isAscending([4,5,6,7,3,7,9]));
+console.log(isAscending([-5, 10, 99, 123456]));
